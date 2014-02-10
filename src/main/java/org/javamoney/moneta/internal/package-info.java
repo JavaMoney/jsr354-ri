6361c7d64309de2de1394a38bce479f1a4597d7d
Anatole Tresch <atsticks@gmail.com>
2014-02-10 21:59:46 +0100

    Added package javadoc.

---
+++ src/main/java/org/javamoney/moneta/internal/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * CREDIT SUISSE IS WILLING TO LICENSE THIS SPECIFICATION TO YOU ONLY UPON THE
+ * CONDITION THAT YOU ACCEPT ALL OF THE TERMS CONTAINED IN THIS AGREEMENT.
+ * PLEASE READ THE TERMS AND CONDITIONS OF THIS AGREEMENT CAREFULLY. BY
+ * DOWNLOADING THIS SPECIFICATION, YOU ACCEPT THE TERMS AND CONDITIONS OF THE
+ * AGREEMENT. IF YOU ARE NOT WILLING TO BE BOUND BY IT, SELECT THE "DECLINE"
+ * BUTTON AT THE BOTTOM OF THIS PAGE. Specification: JSR-354 Money and Currency
+ * API ("Specification") Copyright (c) 2012-2013, Credit Suisse All rights
+ * reserved.
+ */
+
+/**
+ * Contains internal artifacts implementing {@link javax.money.spi.CurrencyProviderSpi}, {@link javax.money.MonetaryAmountFactory}, 
+ * {@link javax.money.spi.MonetaryAmountFactoryProviderSpi} and {@link javax.money.spi.RoundingProviderSpi}.
+ */
+package org.javamoney.moneta.internal;
