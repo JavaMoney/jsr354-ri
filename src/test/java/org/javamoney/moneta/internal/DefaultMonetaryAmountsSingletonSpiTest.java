08d4365a38a4c40cbc14fa58b0cc92e7d5e17ed2
Christian Rudolph <christian+git@famiru.de>
2014-09-06 09:01:28 +0200

    use correct assert method

--- src/test/java/org/javamoney/moneta/internal/DefaultMonetaryAmountsSingletonSpiTest.java
+++ src/test/java/org/javamoney/moneta/internal/DefaultMonetaryAmountsSingletonSpiTest.java
@@ -3,6 +3,7 @@ package org.javamoney.moneta.internal;
 import org.javamoney.moneta.ServicePriority;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class DefaultMonetaryAmountsSingletonSpiTest {
@@ -16,7 +17,7 @@ public class DefaultMonetaryAmountsSingletonSpiTest {
         Object service1 = new Service1();
         Object service2 = new Service2();
         assertTrue(DefaultMonetaryAmountsSingletonSpi.comparePriority(service1, service2) > 0);
-        assertTrue(DefaultMonetaryAmountsSingletonSpi.comparePriority(service1, service1) == 0);
+        assertEquals(DefaultMonetaryAmountsSingletonSpi.comparePriority(service1, service1), 0);
         assertTrue(DefaultMonetaryAmountsSingletonSpi.comparePriority(service2, service1) < 0);
         assertTrue(DefaultMonetaryAmountsSingletonSpi.comparePriority(null, service2) > 0);
     }
