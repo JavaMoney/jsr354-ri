a49014acb42ef7226998a3e486d3df8ee429a30b
atsticks <atsticks@gmail.com>
2015-03-31 14:55:30 +0200

    Fixed compile issue.

--- src/test/java/org/javamoney/moneta/function/ExchangeRateSimpleTest.java
+++ src/test/java/org/javamoney/moneta/function/ExchangeRateSimpleTest.java
@@ -5,15 +5,15 @@ import org.javamoney.moneta.spi.DefaultNumberValue;
 import org.testng.annotations.Test;
 
 import javax.money.CurrencyUnit;
-import javax.money.MonetaryCurrencies;
+import javax.money.Monetary;
 import javax.money.convert.ExchangeRate;
 import javax.money.convert.RateType;
 
 import static org.testng.Assert.assertEquals;
 
 public class ExchangeRateSimpleTest {
-    private static final CurrencyUnit EUR = MonetaryCurrencies.getCurrency("EUR");
-    private static final CurrencyUnit GBP = MonetaryCurrencies.getCurrency("GBP");
+    private static final CurrencyUnit EUR = Monetary.getCurrency("EUR");
+    private static final CurrencyUnit GBP = Monetary.getCurrency("GBP");
 
     @Test
     public void equalsTest() {
