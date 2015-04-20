f06899fb827423cbfa3ea95b244c8ac9ff9f9081
Werner Keil <werner.keil@gmx.net>
2015-04-20 13:12:20 +0200

    Update FastMoney.java
    disabled no-op for now.

--- src/main/java/org/javamoney/moneta/FastMoney.java
+++ src/main/java/org/javamoney/moneta/FastMoney.java
@@ -132,7 +132,8 @@ public final class FastMoney implements MonetaryAmount, Comparable<MonetaryAmoun
      * 
      * @see https://github.com/mongodb/morphia/wiki/EntityAnnotation#name--constructor
      */
-    private FastMoney() {}
+    //private FastMoney() {} commenting out for now
+    // TODO we must find a solution for the FINAL member variables in this case, otherwise the no-op won't work.
 
     /**
      * Creates a new instance os {@link FastMoney}.
