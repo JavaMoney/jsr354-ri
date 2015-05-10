a3837e9292195c919f4c6833434378e17fb5f479
otaviojava <otaviojava@java.net>
2015-05-10 09:58:43 -0300

    Adds FastMoney Producer

---
+++ src/main/java/org/javamoney/moneta/spi/MonetaryAmountProducer.java
@@ -0,0 +1,21 @@
+package org.javamoney.moneta.spi;
+
+import javax.money.CurrencyUnit;
+import javax.money.MonetaryAmount;
+
+/**
+ *The producer of {@link MonetaryAmount} from {@link CurrencyUnit} and {@link Number}
+ * @author Otavio Santana
+ */
+@FunctionalInterface
+public interface MonetaryAmountProducer {
+	/**
+	 * Creates a {@link MonetaryAmount} from {@link CurrencyUnit} and {@link Number}
+	 * @param currency
+	 * @param number
+	 * @return a {@link MonetaryAmount} never null
+	 * @throws NullPointerException if currency and Number is null
+	 */
+	MonetaryAmount create(CurrencyUnit currency, Number number);
+
+}
---
+++ src/test/java/org/javamoney/moneta/spi/FastMoneyProducerTest.java
@@ -0,0 +1,45 @@
+package org.javamoney.moneta.spi;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.Locale;
+
+import javax.money.CurrencyUnit;
+import javax.money.Monetary;
+import javax.money.MonetaryAmount;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FastMoneyProducerTest {
+
+	private MonetaryAmountProducer producer;
+
+	private CurrencyUnit currency;
+
+	@BeforeMethod
+	public void setup() {
+		producer = new FastMoneyProducer();
+		currency = Monetary.getCurrency(Locale.getDefault());
+	}
+
+	@Test(expectedExceptions = NullPointerException.class)
+	public void shouldReturnErrorWhenCurrencyIsNull() {
+		producer.create(null, 10);
+	}
+
+	@Test(expectedExceptions = NullPointerException.class)
+	public void shouldReturnErrorWhenNumberIsNull() {
+		producer.create(currency, null);
+	}
+
+	@Test
+	public void shouldCreateMonetaryAmount() {
+		Long value = 10L;
+		MonetaryAmount amount = producer.create(currency, value);
+		assertEquals(amount.getCurrency(), currency);
+		assertEquals(Long.valueOf(amount.getNumber().longValue()), value);
+
+	}
+
+}
