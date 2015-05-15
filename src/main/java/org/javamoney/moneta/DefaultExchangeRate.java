8d55fe2a4f723daff0a26a0ecdfb6f5cc4c00749
Werner Keil <werner.keil@gmx.net>
2015-05-15 18:05:52 +0400

    Update DefaultExchangeRate.java

--- src/main/java/org/javamoney/moneta/DefaultExchangeRate.java
+++ src/main/java/org/javamoney/moneta/DefaultExchangeRate.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012, 2014, Credit Suisse (Anatole Tresch), Werner Keil and others by the @author tag.
+ * Copyright (c) 2012, 2015, Credit Suisse (Anatole Tresch), Werner Keil and others by the @author tag.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -87,6 +87,7 @@ import javax.money.convert.ExchangeRate;
  * Exchange Rate (Quotations)</a>
  */
 class DefaultExchangeRate implements ExchangeRate, Serializable, Comparable<ExchangeRate> {
+// TODO this should probably go to "convert" in future releases. Analyze feasability of refactoring.
 
     /**
      * serialVersionUID.
