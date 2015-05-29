6dabc94eeca61d4f6ab6d2e986b0ceeb1bdbf27d
Werner Keil <werner.keil@gmx.net>
2015-05-29 23:01:35 +0200

    JAVAMONEY-96: Backport Moved to Shelter since no RI-relevance or use of this SE8 only class Task-Url: https://java.net/jira/browse/JAVAMONEY-96

--- src/test/java/org/javamoney/moneta/internal/convert/IMFHistoricRateProviderTest.java
+++ src/test/java/org/javamoney/moneta/internal/convert/IMFHistoricRateProviderTest.java
@@ -4,24 +4,17 @@ import static javax.money.convert.MonetaryConversions.getExchangeRateProvider;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
-import static org.testng.Assert.fail;
-
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.YearMonth;
 import java.util.Objects;
 
 import javax.money.CurrencyUnit;
 import javax.money.Monetary;
 import javax.money.MonetaryAmount;
-import javax.money.convert.ConversionQuery;
 import javax.money.convert.CurrencyConversion;
 import javax.money.convert.ExchangeRateProvider;
 
 import org.javamoney.moneta.Money;
 import org.javamoney.moneta.convert.ExchangeRateType;
-import org.javamoney.moneta.convert.HistoricConversionQueryBuilder;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -135,70 +128,6 @@ public class IMFHistoricRateProviderTest {
 	        MonetaryAmount result = currencyConversion.apply(money);
 
 	        assertEquals(result.getCurrency(), BRAZILIAN_REAL);
-	        assertTrue(result.getNumber().doubleValue() > 0);
-
-	    }
-
-	    @Test(expectedExceptions = ExchangeRateException.class)
-	    public void shouldReturnErrorWhenDoesNotFindTheExchangeRate() {
-
-
-	        LocalDate localDate = YearMonth.of(2011, Month.JANUARY).atDay(9);
-			ConversionQuery conversionQuery = HistoricConversionQueryBuilder
-					.of(EURO).withDay(localDate).build();
-	        CurrencyConversion currencyConversion = provider
-	                .getCurrencyConversion(conversionQuery);
-	        assertNotNull(currencyConversion);
-	        MonetaryAmount money = Money.of(BigDecimal.TEN, DOLLAR);
-	        currencyConversion.apply(money);
-	        fail();
-	    }
-
-	    @Test
-	    public void shouldFindFromHistoricalUsingPeriod() {
-
-
-	        LocalDate localDate = YearMonth.of(2011, Month.JANUARY).atDay(9);
-			ConversionQuery conversionQuery = HistoricConversionQueryBuilder
-					.of(EURO).withDaysBetween(localDate, localDate.plusDays(10)).build();
-	        CurrencyConversion currencyConversion = provider
-	                .getCurrencyConversion(conversionQuery);
-	        assertNotNull(currencyConversion);
-	        MonetaryAmount money = Money.of(BigDecimal.TEN, DOLLAR);
-	        MonetaryAmount result = currencyConversion.apply(money);
-
-	        assertEquals(result.getCurrency(), EURO);
-	        assertTrue(result.getNumber().doubleValue() > 0);
-	    }
-
-	    @Test(expectedExceptions = ExchangeRateException.class)
-	    public void shouldReturnErrorWhenFindFromHistoricalUsingPeriod() {
-
-
-	        LocalDate localDate = YearMonth.of(2011, Month.JANUARY).atDay(9);
-			ConversionQuery conversionQuery = HistoricConversionQueryBuilder
-					.of(EURO).withDaysBetween(localDate.minusDays(1), localDate).build();
-	        CurrencyConversion currencyConversion = provider
-	                .getCurrencyConversion(conversionQuery);
-	        assertNotNull(currencyConversion);
-	        MonetaryAmount money = Money.of(BigDecimal.TEN, DOLLAR);
-	        currencyConversion.apply(money);
-	        fail();
-	    }
-
-	    @Test
-	    public void shouldSetTimeInLocalDateTime() {
-
-	        LocalDate localDate = YearMonth.of(2014, Month.JANUARY).atDay(9);
-	        ConversionQuery conversionQuery = HistoricConversionQueryBuilder
-					.of(EURO).withDay(localDate).build();
-	        CurrencyConversion currencyConversion = provider
-	                .getCurrencyConversion(conversionQuery);
-	        assertNotNull(currencyConversion);
-	        MonetaryAmount money = Money.of(BigDecimal.TEN, DOLLAR);
-	        MonetaryAmount result = currencyConversion.apply(money);
-
-	        assertEquals(result.getCurrency(), EURO);
 	        assertTrue(result.getNumber().doubleValue() > 0);
 
 	    }
