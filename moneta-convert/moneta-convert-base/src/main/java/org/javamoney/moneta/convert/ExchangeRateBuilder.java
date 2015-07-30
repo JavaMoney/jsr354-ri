9922046f5eadf8e5db6c4f23398590acfd046e23
Werner Keil <werner.keil@gmx.net>
2015-07-31 00:47:25 +0200

    Updated header

--- moneta-convert/moneta-convert-base/src/main/java/org/javamoney/moneta/convert/ExchangeRateBuilder.java
+++ moneta-convert/moneta-convert-base/src/main/java/org/javamoney/moneta/convert/ExchangeRateBuilder.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2012, 2015, Anatole Tresch, Werner Keil and others by the @author tag.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.javamoney.moneta.convert;
 
 import javax.money.CurrencyUnit;
@@ -19,7 +34,7 @@ import java.util.Objects;
  * @author Werner Keil
  */
 public class ExchangeRateBuilder {
-// TODO this should probably go to "convert" in future releases. Analyze feasability of refactoring.
+
     /**
      * The {@link javax.money.convert.ConversionContext}.
      */
