3bbd40b06c4d7ffb125a3bfee80ac6807698f149
Werner Keil <werner.keil@gmx.net>
2015-08-27 12:27:49 +0200

    Update DefaultExchangeRate.java
    @deprecated in JavaDoc is enough

--- src/main/java/org/javamoney/moneta/DefaultExchangeRate.java
+++ src/main/java/org/javamoney/moneta/DefaultExchangeRate.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012, 2015, Credit Suisse (Anatole Tresch), Werner Keil and others by the @author tag.
+ * Copyright (c) 2012, 2015, Credit Suisse, Anatole Tresch, Werner Keil and others by the @author tag.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -87,7 +87,6 @@ import javax.money.convert.ExchangeRate;
  * Exchange Rate (Quotations)</a>
  * @deprecated Use org.javamoney.moneta.convert.DefaultExchangeRate instead
  */
-@Deprecated
 class DefaultExchangeRate implements ExchangeRate, Serializable, Comparable<ExchangeRate> {
 // TODO this should probably go to "convert" in future releases. Analyze feasability of refactoring.
 
