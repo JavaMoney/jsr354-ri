77e2d2b6c4cd76792c6cf4e93fc506eb906c552f
Werner Keil <werner.keil@gmx.net>
2015-08-27 12:28:34 +0200

    Update ExchangeRateBuilder.java
    @deprecated in JavaDoc is enough

--- src/main/java/org/javamoney/moneta/ExchangeRateBuilder.java
+++ src/main/java/org/javamoney/moneta/ExchangeRateBuilder.java
@@ -19,9 +19,7 @@ import java.util.Objects;
  * @author Werner Keil
  * @deprecated Use org.javamoney.moneta.convert.ExchangeRateBuilder instead
  */
-@Deprecated
 public class ExchangeRateBuilder {
-// TODO this should probably go to "convert" in future releases. Analyze feasability of refactoring.
     /**
      * The {@link javax.money.convert.ConversionContext}.
      */
