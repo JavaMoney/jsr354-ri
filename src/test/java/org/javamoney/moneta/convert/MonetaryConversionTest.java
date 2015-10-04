44919ac4120f561af0e90c7a3387e19ab452c341
Werner Keil <werner.keil@gmx.net>
2015-10-04 17:14:09 +0200

    Added Unit Tests

--- src/test/java/org/javamoney/moneta/convert/MonetaryConversionTest.java
+++ src/test/java/org/javamoney/moneta/convert/MonetaryConversionTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012, 2014, Credit Suisse (Anatole Tresch), Werner Keil and others by the @author tag.
+ * Copyright (c) 2012, 2015, Anatole Tresch, Werner Keil and others by the @author tag.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -24,24 +24,27 @@ import java.util.Collection;
 import java.util.Objects;
 
 import javax.money.Monetary;
+import javax.money.MonetaryAmount;
+import javax.money.convert.CurrencyConversion;
 import javax.money.convert.ExchangeRate;
 import javax.money.convert.ExchangeRateProvider;
 import javax.money.convert.MonetaryConversions;
 
+import org.javamoney.moneta.Money;
 import org.javamoney.moneta.internal.convert.ECBCurrentRateProvider;
 import org.javamoney.moneta.spi.CompoundRateProvider;
 import org.testng.annotations.Test;
 
 public class MonetaryConversionTest {
 
-    @Test
-    public void testGetExchangeRateDefault() {
-        ExchangeRateProvider prov = MonetaryConversions
-                .getExchangeRateProvider();
-        assertTrue(Objects.nonNull(prov));
-        ExchangeRate rate = prov.getExchangeRate("CHF", "EUR");
-        assertNotNull(rate);
-    }
+	@Test
+	public void testGetExchangeRateDefault() {
+		ExchangeRateProvider prov = MonetaryConversions
+				.getExchangeRateProvider();
+		assertTrue(Objects.nonNull(prov));
+		ExchangeRate rate = prov.getExchangeRate("CHF", "EUR");
+		assertNotNull(rate);
+	}
 
 	@Test
 	public void testGetExchangeRateProvider() {
@@ -52,15 +55,15 @@ public class MonetaryConversionTest {
 	}
 
 	@Test
-	public void testGetExchangeRateProvider_Chained() throws InterruptedException {
+	public void testGetExchangeRateProvider_Chained()
+			throws InterruptedException {
 		ExchangeRateProvider prov = MonetaryConversions
 				.getExchangeRateProvider("ECB", "IMF");
 		assertTrue(Objects.nonNull(prov));
 		assertEquals(CompoundRateProvider.class, prov.getClass());
 		// Test rate provided by IMF (derived)
 		Thread.sleep(5000L); // wait for provider to load...
-		ExchangeRate r = prov.getExchangeRate(
-				Monetary.getCurrency("USD"),
+		ExchangeRate r = prov.getExchangeRate(Monetary.getCurrency("USD"),
 				Monetary.getCurrency("INR"));
 		assertTrue(Objects.nonNull(r));
 		assertTrue(r.isDerived());
@@ -73,11 +76,27 @@ public class MonetaryConversionTest {
 
 	@Test
 	public void testGetSupportedProviderContexts() {
-		Collection<String> types = MonetaryConversions.getConversionProviderNames();
+		Collection<String> types = MonetaryConversions
+				.getConversionProviderNames();
 		assertNotNull(types);
 		assertTrue(types.size() >= 1);
 		assertTrue(types.contains("IMF"));
 		assertTrue(types.contains("ECB"));
 	}
 
+	@Test
+	public void testMonetaryConversionsECB() {
+		final MonetaryAmount amt = Money.of(2000, "EUR");
+		CurrencyConversion conv= MonetaryConversions.getConversion("CHF", "ECB");
+		MonetaryAmount converted = amt.with(conv);
+		assertNotNull(converted);
+	}
+	
+	@Test
+	public void testMonetaryConversionsIMF() {
+		final MonetaryAmount amt = Money.of(2000, "EUR");
+		CurrencyConversion conv= MonetaryConversions.getConversion("CHF", "IMF");
+		MonetaryAmount converted = amt.with(conv);
+		assertNotNull(converted);
+	}
 }
