6e61c07cd09d7e58bb336c8467c43d4c9af6eedb
Otavio <otaviojava@java.net>
2016-01-09 11:17:08 -0200

    Put deprecated constructor in RoundedMoney

--- src/main/java/org/javamoney/moneta/RoundedMoney.java
+++ src/main/java/org/javamoney/moneta/RoundedMoney.java
@@ -215,6 +215,7 @@ public final class RoundedMoney implements MonetaryAmount, Comparable<MonetaryAm
      * @param number       The numeric part, not null.
      * @return A new instance of {@link RoundedMoney}.
      */
+    @Deprecated
     public static RoundedMoney of(Number number, String currencyCode) {
         return new RoundedMoney(number, Monetary.getCurrency(currencyCode),
                 Monetary.getDefaultRounding());
