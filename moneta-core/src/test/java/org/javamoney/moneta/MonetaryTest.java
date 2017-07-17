bf23628feb909db7133b12d6b8ce97d6dcf0ad9e
Werner Keil <werner.keil@gmx.net>
2017-07-17 23:36:24 +0200

    162: Question regarding creation of MonetaryAmount
    Task-Url: https://github.com/JavaMoney/jsr354-ri/issues/issues/162

--- moneta-core/src/test/java/org/javamoney/moneta/MonetaryTest.java
+++ moneta-core/src/test/java/org/javamoney/moneta/MonetaryTest.java
@@ -15,62 +15,65 @@
  */
 package org.javamoney.moneta;
 
-
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNull;
 
+import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 import javax.money.Monetary;
 import javax.money.MonetaryAmount;
 import javax.money.MonetaryAmountFactoryQueryBuilder;
+import javax.money.MonetaryContext;
+import javax.money.MonetaryContextBuilder;
 
+import org.junit.Ignore;
 import org.testng.annotations.Test;
 
 public class MonetaryTest {
 
 	@Test
 	public void shouldCreateMonetaryFactory() {
-		MonetaryAmount monetaryAmount = Monetary.getAmountFactory(
-                MonetaryAmountFactoryQueryBuilder.of()
-                .set(RoundingMode.DOWN)
-                .setPrecision(256).build()
-        ).setCurrency("CHF").setNumber(1234.5678).create();
-        assertEquals(256, monetaryAmount.getContext().getPrecision());
-        assertEquals(RoundingMode.DOWN, monetaryAmount.getContext().get(RoundingMode.class));
+		MonetaryAmount monetaryAmount = Monetary
+				.getAmountFactory(
+						MonetaryAmountFactoryQueryBuilder.of().set(RoundingMode.DOWN).setPrecision(256).build())
+				.setCurrency("CHF").setNumber(1234.5678).create();
+		assertEquals(256, monetaryAmount.getContext().getPrecision());
+		assertEquals(RoundingMode.DOWN, monetaryAmount.getContext().get(RoundingMode.class));
 	}
 
 	@Test
 	public void shouldCreateMonetaryFactoryWithRoundindModeNull() {
-		MonetaryAmount monetaryAmount = Monetary.getAmountFactory(
-                MonetaryAmountFactoryQueryBuilder.of()
-                .setPrecision(256).build()
-        ).setCurrency("CHF").setNumber(1234.5678).create();
-        assertEquals(256, monetaryAmount.getContext().getPrecision());
-        assertNull(monetaryAmount.getContext().get(RoundingMode.class));
+		MonetaryAmount monetaryAmount = Monetary
+				.getAmountFactory(MonetaryAmountFactoryQueryBuilder.of().setPrecision(256).build()).setCurrency("CHF")
+				.setNumber(1234.5678).create();
+		assertEquals(256, monetaryAmount.getContext().getPrecision());
+		assertNull(monetaryAmount.getContext().get(RoundingMode.class));
 	}
 
 	@Test
 	public void shouldCreateMonetaryFactoryWithPrecisionNull() {
 		MonetaryAmount monetaryAmount = Monetary.getAmountFactory(
-                MonetaryAmountFactoryQueryBuilder.of()
-                .set(RoundingMode.HALF_DOWN)
-                .setTargetType(Money.class)
-                .build()
-        ).setCurrency("CHF").setNumber(1234.5678).create();
+				MonetaryAmountFactoryQueryBuilder.of().set(RoundingMode.HALF_DOWN).setTargetType(Money.class).build())
+				.setCurrency("CHF").setNumber(1234.5678).create();
 		assertEquals(0, monetaryAmount.getContext().getPrecision());
-        assertEquals(RoundingMode.HALF_DOWN, monetaryAmount.getContext().get(RoundingMode.class));
+		assertEquals(RoundingMode.HALF_DOWN, monetaryAmount.getContext().get(RoundingMode.class));
 	}
 
 	@Test
 	public void shouldCreateMonetaryFactoryWithBothNull() {
 		MonetaryAmount monetaryAmount = Monetary.getAmountFactory(
-                MonetaryAmountFactoryQueryBuilder.of()
-                .set(RoundingMode.HALF_DOWN)
-                .setTargetType(Money.class)
-                .build()
-        ).setCurrency("CHF").setNumber(1234.5678).create();
+				MonetaryAmountFactoryQueryBuilder.of().set(RoundingMode.HALF_DOWN).setTargetType(Money.class).build())
+				.setCurrency("CHF").setNumber(1234.5678).create();
 		assertEquals(0, monetaryAmount.getContext().getPrecision());
-        assertEquals(RoundingMode.HALF_DOWN, monetaryAmount.getContext().get(RoundingMode.class));
+		assertEquals(RoundingMode.HALF_DOWN, monetaryAmount.getContext().get(RoundingMode.class));
+	}
+
+	@Test(enabled=false)
+	// TODO Currently maxScale is not used in some places, see https://github.com/JavaMoney/jsr354-ri/issues/162
+	public void shouldUseContextInFactory() {
+		MonetaryContext mc = MonetaryContextBuilder.of().setMaxScale(2).setPrecision(64).set(RoundingMode.HALF_DOWN).build();
+		MonetaryAmount am = Monetary.getDefaultAmountFactory().setContext(mc).setNumber(999.999).setCurrency("EUR").create();
+		assertEquals(am.getNumber(), BigDecimal.valueOf(999.99));
 	}
 }
