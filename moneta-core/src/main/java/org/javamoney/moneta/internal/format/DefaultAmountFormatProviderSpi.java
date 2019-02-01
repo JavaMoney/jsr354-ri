JavaMoney/jsr354-ri

commit ca7a20ab2d5f86bdc0fb86699d39243d63e13bff

DefaultAmountFormatProviderSpi: cleanup and speedup

moneta-core/src/main/java/org/javamoney/moneta/internal/format/DefaultAmountFormatProviderSpi.java
@@ -17,18 +17,20 @@ package org.javamoney.moneta.internal.format;
 
 
 import java.text.DecimalFormat;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.money.format.AmountFormatContext;
 import javax.money.format.AmountFormatContextBuilder;
 import javax.money.format.AmountFormatQuery;
 import javax.money.format.MonetaryAmountFormat;
 import javax.money.spi.MonetaryAmountFormatProviderSpi;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.*;
 
 /**
  * Default format provider, which mainly maps the existing JDK functionality into the JSR 354 logic.
@@ -40,14 +42,12 @@ public class DefaultAmountFormatProviderSpi implements MonetaryAmountFormatProvi
     private static final String DEFAULT_STYLE = "default";
     private static final String PROVIDER_NAME = "default";
 
-    private Set<Locale> supportedSets = new HashSet<>();
-    private Set<String> formatNames = new HashSet<>();
+    private final Set<Locale> supportedSets;
+    private final Set<String> formatNames ;
 
     public DefaultAmountFormatProviderSpi() {
-        supportedSets.addAll(Arrays.asList(DecimalFormat.getAvailableLocales()));
-        supportedSets = Collections.unmodifiableSet(supportedSets);
-        formatNames.add(DEFAULT_STYLE);
-        formatNames = Collections.unmodifiableSet(formatNames);
+        supportedSets = unmodifiableSet(new HashSet<>(asList(DecimalFormat.getAvailableLocales())));
+        formatNames = singleton(DEFAULT_STYLE);
     }
 
     @Override
@@ -56,19 +56,18 @@ public class DefaultAmountFormatProviderSpi implements MonetaryAmountFormatProvi
     }
 
     /*
-         * (non-Javadoc)
-         * @see
-         * javax.money.spi.MonetaryAmountFormatProviderSpi#getFormat(javax.money.format.AmountFormatContext)
-         */
+     * (non-Javadoc)
+     * @see
+     * javax.money.spi.MonetaryAmountFormatProviderSpi#getFormat(javax.money.format.AmountFormatContext)
+     */
     @Override
     public Collection<MonetaryAmountFormat> getAmountFormats(AmountFormatQuery amountFormatQuery) {
         Objects.requireNonNull(amountFormatQuery, "AmountFormatContext required");
-        if (!amountFormatQuery.getProviderNames().isEmpty() &&
-                !amountFormatQuery.getProviderNames().contains(getProviderName())) {
-            return Collections.emptySet();
+        if (!amountFormatQuery.getProviderNames().contains(getProviderName())) {
+            return emptySet();
         }
-        if (!(amountFormatQuery.getFormatName() == null || DEFAULT_STYLE.equals(amountFormatQuery.getFormatName()))) {
-            return Collections.emptySet();
+        if (amountFormatQuery.getFormatName() != null && !DEFAULT_STYLE.equals(amountFormatQuery.getFormatName())) {
+            return emptySet();
         }
         AmountFormatContextBuilder builder = AmountFormatContextBuilder.of(DEFAULT_STYLE);
         if (amountFormatQuery.getLocale() != null) {
@@ -76,7 +75,8 @@ public class DefaultAmountFormatProviderSpi implements MonetaryAmountFormatProvi
         }
         builder.importContext(amountFormatQuery, false);
         builder.setMonetaryAmountFactory(amountFormatQuery.getMonetaryAmountFactory());
-        return Arrays.asList(new MonetaryAmountFormat[]{new DefaultMonetaryAmountFormat(builder.build())});
+        AmountFormatContext amountFormatContext = builder.build();
+        return asList(new MonetaryAmountFormat[]{new DefaultMonetaryAmountFormat(amountFormatContext)});
     }
 
     @Override

commit ee4dc76764545a5eb2634233239a02c21b1a977e

Fix a bug introduced in ca7a20ab

moneta-core/src/main/java/org/javamoney/moneta/internal/format/DefaultAmountFormatProviderSpi.java
@@ -20,7 +20,6 @@ import java.text.DecimalFormat;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Locale;
-import java.util.Objects;
 import java.util.Set;
 
 import javax.money.format.AmountFormatContext;
@@ -31,6 +30,7 @@ import javax.money.spi.MonetaryAmountFormatProviderSpi;
 
 import static java.util.Arrays.asList;
 import static java.util.Collections.*;
+import static java.util.Objects.requireNonNull;
 
 /**
  * Default format provider, which mainly maps the existing JDK functionality into the JSR 354 logic.
@@ -62,8 +62,9 @@ public class DefaultAmountFormatProviderSpi implements MonetaryAmountFormatProvi
      */
     @Override
     public Collection<MonetaryAmountFormat> getAmountFormats(AmountFormatQuery amountFormatQuery) {
-        Objects.requireNonNull(amountFormatQuery, "AmountFormatContext required");
-        if (!amountFormatQuery.getProviderNames().contains(getProviderName())) {
+        requireNonNull(amountFormatQuery, "AmountFormatContext required");
+        if (!amountFormatQuery.getProviderNames().isEmpty() &&
+                !amountFormatQuery.getProviderNames().contains(getProviderName())) {
             return emptySet();
         }
         if (amountFormatQuery.getFormatName() != null && !DEFAULT_STYLE.equals(amountFormatQuery.getFormatName())) {