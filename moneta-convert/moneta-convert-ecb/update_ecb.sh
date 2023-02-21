2686bc07d62c18718f0665de701a331566daa8f4
Werner Keil <werner.keil@gmx.net>
2023-02-21 23:36:59 +0100

    Refactoring conversion data

--- moneta-convert/moneta-convert-ecb/update_ecb.sh
+++ moneta-convert/moneta-convert-ecb/update_ecb.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 printf "\n Downloading the ECB daily resource. \n"
-wget https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml
-mv eurofxref-daily.xml src/main/resources/org/javamoney/moneta/convert/ecb/defaults/eurofxref-daily.xml
+# wget https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml
+#mv eurofxref-daily.xml src/main/resources/org/javamoney/moneta/convert/ecb/defaults/eurofxref-daily.xml
+curl --fail "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml" \
+		--output src/main/resources/org/javamoney/moneta/convert/ecb/defaults/eurofxref-daily.xml
 printf "\n Done. Downloading the ECB-90 resource. \n"
-wget https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml
-mv eurofxref-hist-90d.xml src/main/resources/org/javamoney/moneta/convert/ecb/defaults/eurofxref-hist-90d.xml
+# wget https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml
+# mv eurofxref-hist-90d.xml src/main/resources/org/javamoney/moneta/convert/ecb/defaults/eurofxref-hist-90d.xml
+curl --fail "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml" \
+		--output src/main/resources/org/javamoney/moneta/convert/ecb/defaults/eurofxref-hist-90d.xml
 printf "\n Done. Downloading the ECB-hist resource. \n"
-wget https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml
-mv eurofxref-hist.xml src/main/resources/org/javamoney/moneta/convert/ecb/defaults/eurofxref-hist.xml
+# wget https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml
+# mv eurofxref-hist.xml src/main/resources/org/javamoney/moneta/convert/ecb/defaults/eurofxref-hist.xml
+curl --fail "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml" \
+		--output src/main/resources/org/javamoney/moneta/convert/ecb/defaults/eurofxref-hist.xml
 printf "\n Done.
