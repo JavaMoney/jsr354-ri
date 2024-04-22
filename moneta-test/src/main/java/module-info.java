6ca9cced920eebf4fbc4198dab272d3f71c07bc8
João Cabrita <kewne@protonmail.com>
2024-04-22 14:24:00 +0100

    fix: rename test module name

--- moneta-test/src/main/java/module-info.java
+++ moneta-test/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 
-module org.javamoney.moneta.test_extension {
+module org.javamoney.moneta.test {
     requires java.money;
     requires org.javamoney.moneta;
     provides javax.money.spi.RoundingProviderSpi with org.javamoney.moneta.test.TestRoundingProvider;
