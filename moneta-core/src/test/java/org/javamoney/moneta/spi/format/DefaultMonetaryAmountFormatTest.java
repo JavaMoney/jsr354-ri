2478ea736ac1d9cf2d30ebfbd7c6fd665977a4fd
Werner Keil <werner.keil@gmx.net>
2025-04-05 20:55:35 +0200

    Updated JDK and Locale handling in Unit Tests

--- moneta-core/src/test/java/org/javamoney/moneta/spi/format/DefaultMonetaryAmountFormatTest.java
+++ moneta-core/src/test/java/org/javamoney/moneta/spi/format/DefaultMonetaryAmountFormatTest.java
@@ -157,7 +157,7 @@ public class DefaultMonetaryAmountFormatTest {
         final String[] skipLangArray = {"as", "ar", "bn", "ckb", "dz", "fa", "ig", "ks", "lrc",
                 "mni", "mr", "my", "mzn", "ne", "pa", "ps", "sa", "sat", "sd", "th", "ur", "uz", "raj", "bgc", "bho"};
         final Set<String> SKIPPED_LANGUAGES = new HashSet<>(Arrays.asList(skipLangArray));
-        final Locale[] locArray = new Locale[]{Locale.of("dz", "BT")
+        final Locale[] locArray = new Locale[]{new Locale("dz", "BT")
         };
         final Set<Locale> SKIPPED_LOCALES = new HashSet<>(Arrays.asList(locArray));
 
